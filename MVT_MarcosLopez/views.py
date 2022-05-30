3beafc9ab0175c995776f4e1ef074aa5cc58eff2
MLopez <programadormlopez@gmail.com>
2022-05-29 23:38:42 -0300

    commit mvt

--- MVT_MarcosLopez/views.py
+++ MVT_MarcosLopez/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 def saludo(request):
     return HttpResponse('hola mundo')
 
 def probando_template(request):
-    return render(request,'index.html',context = {})
+    context = {
+        'nombre':'Marcos',
+        'apellido':'Lopez',
+        'fecha':datetime.now().date,
+        'edades':[1,2,3,4,5,6,7,8,9,10]
+    }
+    return render(request,'index.html',context = context)
---
+++ familiares/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from familiares.models import familiares
+
+# Register your models here.
+
+admin.site.register(familiares)
